9d371794e9a4661ff4b17fc4745afacddcc75b56
rohan <rohan@Rohan>
2022-07-14 17:09:04 +0530

    first commit

---
+++ Day2_intro_java/src/com/cg/demo/program1.java
@@ -0,0 +1,17 @@
+package com.cg.demo;
+
+import java.util.Scanner;
+
+public class program1 {
+
+	private static final String Syetem = null;
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println("Value of a:"+a);
+		
+
+	}
+
+}
