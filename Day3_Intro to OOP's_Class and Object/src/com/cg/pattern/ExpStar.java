2c773186be4cec05d7e36bbc5f4348df53480766
rohan <rohan@Rohan>
2022-07-16 19:50:02 +0530

    Second Commit

---
+++ Day3_Intro to OOP's_Class and Object/src/com/cg/pattern/ExpStar.java
@@ -0,0 +1,20 @@
+package com.cg.pattern;
+
+import java.util.Scanner;
+
+public class ExpStar {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.print("*"+" ");
+		}
+		s.close();
+		
+		
+	}
+
+}
+ 
---
+++ Day3_Intro to OOP's_Class and Object/src/com/cg/pattern/ExpStra3_Right.java
@@ -0,0 +1,19 @@
+package com.cg.pattern;
+
+import java.util.Scanner;
+
+public class ExpStra3_Right {
+	Scanner s = new Scanner(System.in);
+	int n=s.nextInt();{
+	for(int i=n;i<=n;++i)
+	{
+		for(int j=n;j<=i;++j) {
+			System.out.print("*"+" ");
+		}
+		System.out.println();
+	}
+	s.close();
+	}
+	
+
+}
